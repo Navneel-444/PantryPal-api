3de88af0a2723ca9372103fcaf1e8e0ef4f59cae
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-10 01:15:46 -0400

    create meal table

--- migrations/20240410044615_create_meal_table.js
+++ migrations/20240410044615_create_meal_table.js
@@ -2,14 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable("day", (table) => {
+        table.increments("id").primary();
+        table.string("meal_times").notNullable();
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table
+            .timestamp("updated_at")
+            .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"));
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTable("day");
+
 };
